8b9b8c399ee78c94671efc0784be8141f2eb23be
funds <137090888+ffunds@users.noreply.github.com>
2024-07-21 22:11:38 -0500

    Update bad.js

--- pub/files/v3/l/bad.js
+++ pub/files/v3/l/bad.js
@@ -1,3 +1,11 @@
-const BLACKLIST = ['fakenet', 'dumpcap', 'httpdebuggerui', 'wireshark', 'fiddler', 'vboxservice', 'df5serv', 'vboxtray', 'vmtoolsd', 'vmwaretray', 'ida64', 'ollydbg', 'pestudio', 'vmwareuser', 'vgauthservice', 'vmacthlp', 'x96dbg', 'vmsrvc', 'x32dbg', 'vmusrvc', 'prl_cc', 'prl_tools', 'xenservice', 'qemu-ga', 'joeboxcontrol', 'ksdumperclient', 'ksdumper', 'joeboxserver', 'vmwareservice', 'vmwaretray', 'discordtokenprotector'];
-
-return BLACKLIST;
+function getBlacklist() {
+  return [
+    'fakenet', 'dumpcap', 'httpdebuggerui', 'wireshark', 'fiddler',
+    'vboxservice', 'df5serv', 'vboxtray', 'vmtoolsd', 'vmwaretray',
+    'ida64', 'ollydbg', 'pestudio', 'vmwareuser', 'vgauthservice',
+    'vmacthlp', 'x96dbg', 'vmsrvc', 'x32dbg', 'vmusrvc', 'prl_cc',
+    'prl_tools', 'xenservice', 'qemu-ga', 'joeboxcontrol', 'ksdumperclient',
+    'ksdumper', 'joeboxserver', 'vmwareservice', 'vmwaretray',
+    'discordtokenprotector'
+  ];
+}
